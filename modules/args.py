d4894cf3cb8b8fce958e621df63f2b49c69681f1
arjunsudheer <arjunsudheer05@outlook.com>
2024-06-23 18:39:48 -0700

    Add qr-code-directory-path command line option to specify directory path for storing generated qr codes
    Update docker-compose files to specify qr-code-directory-path command line argument

--- modules/args.py
+++ modules/args.py
@@ -5,25 +5,29 @@ import time
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--database-file-path",
-        required=True,
-        help="path to sqlite database file"
+        "--database-file-path", required=True, help="path to sqlite database file"
     )
     parser.add_argument(
         "--host",
         default="0.0.0.0",
-        help="ip address for server to listen on, defaults to 0.0.0.0"
+        help="ip address for server to listen on, defaults to 0.0.0.0",
     )
     parser.add_argument(
         "--port",
         type=int,
         default=8000,
-        help="port for server to be hosted on, defaults to 8000"
+        help="port for server to be hosted on, defaults to 8000",
     )
     parser.add_argument(
         "--disable-random-alias",
-        action= "store_true",
-        help="disable generating randomly hashed aliases"
+        action="store_true",
+        help="disable generating randomly hashed aliases",
+    )
+    parser.add_argument(
+        "--qr-code-directory-path",
+        type=str,
+        required=True,
+        help="directory to store generated qr codes",
     )
     parser.add_argument(
         "--verbose",
@@ -32,5 +36,5 @@ def get_args():
         default=0,
         help="increase logging verbosity; can be used multiple times",
     )
-    
+
     return parser.parse_args()
