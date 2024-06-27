7061f3c303f5945e157dd82fac17a01f0e1981fe
syng828 <117144719+syng828@users.noreply.github.com>
2024-06-26 22:15:38 -0700

    Qrcode generation (#39)
    * initial qrcode generation code. only works in qr_code.py
    
    quick change to work on server.py
    
    removed comment & changed qr endpt
    
    Update modules/args.py
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
    
    Update server.py
    
    syntax change
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
    
    change to custom base url
    
    moved folder and max size to constructor
    
    name fix
    
    Update modules/qr_code.py
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
    
    Update modules/qr_code.py
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
    
    Update server.py
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
    
    Update modules/qr_code.py
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
    
    syntax fix and exceed size fix
    
    changed to give file response
    
    Update modules/qr_code.py
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
    
    Update modules/qr_code.py
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
    
    cache logic fix
    
    syntax fix
    
    added qr to nginx
    
    add args in yml
    
    use tmp
    
    changed to get request for qr endpt
    
    whoops
    
    * syng828 greates vp of all time
    
    ---------
    
    Co-authored-by: evan ugarte <evanuxd@gmail.com>

---
+++ modules/qr_code.py
@@ -0,0 +1,55 @@
+import qrcode
+import uuid
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+class QRCode:
+    def __init__(self, base_url, qr_cache_path, max_size) -> None:
+        self.mapping = {}
+        self.base_url = base_url
+        self.qr_cache_path = qr_cache_path
+        self.max_size = max_size
+
+    def add(self, alias: str):
+        try:    
+            if len(self.mapping) >= self.max_size: #removes files if exceeds size
+                remove_alias,remove_path = self.mapping.popitem()
+                os.remove(remove_path)
+                logger.debug(f"Removed qrcode with alias: {remove_alias} to free space.")
+                
+            url = os.path.join(self.base_url, alias) 
+            path = os.path.join(self.qr_cache_path, str(uuid.uuid4()) + ".jpg")
+
+            img = qrcode.make(url)
+            img.save(path)
+            self.mapping[alias] = path
+
+            return path
+        except FileNotFoundError:
+            logger.exception(f"Could not find folder {self.qr_cache_path}:")
+        except OSError:
+            logger.exception(f"Error occured when handling files:")
+        except Exception:
+            logger.exception(f"An unexpected error occured")
+
+    def find(self, alias: str):
+        return self.mapping.get(alias)
+
+    def delete(self, alias: str):
+        path = self.mapping.get(alias)
+        if alias in self.mapping:
+            self.mapping.pop(alias)
+        if os.path.exists(path):
+            os.remove(path)
+            logger.debug(f"removed qr code at {path} for alias {alias}")
+
+    def clear(self):
+        try: 
+            for alias in self.mapping.keys():
+                self.delete(alias)
+            self.mapping.clear()
+            logger.debug("Cleared qr code folder")
+        except Exception:
+            logger.exception("An unexpected error occurred clearing the cache")
